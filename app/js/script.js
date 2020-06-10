d18a1b6b874802011fb6f5dfdda8ce357452d488
niksolid <semenov.send@gmail.com>
2020-06-10 18:36:18 +0300

    Написал функцию scrollWidthFunction для flex сетки, она нужна для того чтобы выводить при заданной ширине страницы ограниченное количество статей, без нее flex-wrap и flex-grow в совокупности не работают как нужно.

---
+++ app/js/script.js
@@ -0,0 +1,45 @@
+; (function () {
+
+  let articleSection = document.querySelector('.article');
+
+  let articleItems = articleSection.querySelectorAll('.article__item')
+  console.log(articleItems.length);
+
+  scrollWidthFunction();
+  window.addEventListener("resize", scrollWidthFunction);
+
+  function scrollWidthFunction() {
+    let scrollWidth = document.body.scrollWidth;
+    let allClearfix = articleSection.querySelectorAll('.clearfix');
+    console.log(allClearfix.length);
+    for (let clearfix of allClearfix) {
+      clearfix.remove();
+      console.log('del clearfix');
+    }
+    if (scrollWidth >= 1240) {
+      for (let i = 2; i <= articleItems.length - 1; i = i + 3) {
+        let lineBreak = document.createElement('div');
+        lineBreak.classList.add('clearfix');
+        lineBreak.classList.add('maxWidth');
+        articleItems[i].after(lineBreak);
+      }
+    } else if (scrollWidth >= 768) {
+      for (let i = 1; i <= articleItems.length - 1; i = i + 2) {
+
+        let lineBreak = document.createElement('div');
+        lineBreak.classList.add('clearfix');
+        lineBreak.classList.add('maxWidth');
+        articleItems[i].after(lineBreak);
+      }
+    } else {
+      for (let i = 0; i <= articleItems.length -1; i++) {
+        let lineBreak = document.createElement('div');
+        lineBreak.classList.add('clearfix');
+        lineBreak.classList.add('maxWidth');
+        articleItems[i].after(lineBreak);
+      }
+    }
+  }
+
+
+})();
