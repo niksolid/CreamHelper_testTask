aaecc36bd3414fff0a36390c67eb4ea478aad99b
niksolid <semenov.send@gmail.com>
2020-06-11 02:05:53 +0300

    Настроена кнопка, добавляющая больше статей при нажатии. Если статьи отсутствуют - кнопка исчезает

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,18 @@
 ; (function () {
 
   let articleSection = document.querySelector('.article');
+  let articleItems = articleSection.querySelectorAll('.article__item');
+  let moreBtn = document.querySelector('.article-more__btn');
 
-  let articleItems = articleSection.querySelectorAll('.article__item')
-  console.log(articleItems.length);
-
+  moreArticles();
   scrollWidthFunction();
-  window.addEventListener("resize", scrollWidthFunction);
 
+  moreBtn.onclick = function() {
+    moreArticles();
+  }
+
+  window.addEventListener("resize", scrollWidthFunction);
+  // функция переноса строки в зависимости от ширины экрана
   function scrollWidthFunction() {
     let scrollWidth = document.body.scrollWidth;
     let allClearfix = articleSection.querySelectorAll('.clearfix');
@@ -38,6 +43,18 @@
         lineBreak.classList.add('maxWidth');
         articleItems[i].after(lineBreak);
       }
+    }
+  };
+
+  // функция добавления статей
+  function moreArticles() {
+    noActiveArticle = document.querySelectorAll('.article__no-active');
+    for (let i = 0; i <= noActiveArticle.length -1 && i < 6; i++){
+      noActiveArticle[i].classList.remove('article__no-active');
+      noActiveArticle[i].classList.add('is-active');      
+    }
+    if (noActiveArticle.length <= 6 ) {
+      moreBtn.classList.add('hidden-Element');
     }
   }
 
